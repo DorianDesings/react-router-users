c6ea434ca94c9d53c352c95bbad1dd40790043d8
Dorian <dorianoriginaldesings@gmail.com>
2023-04-06 00:37:25 +0200

    Global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { BrowserRouter } from 'react-router-dom';
 import Router from './router/Router';
+import { GlobalStyles } from './styles/GlobalStyles';
 
 const App = () => (
 	<BrowserRouter>
+		<GlobalStyles />
 		<Router />
 	</BrowserRouter>
 );
